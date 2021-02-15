7d8068cdea7fb06a1b4567b15798c8e3bf9753a0
romeo adjam <sodangbe@users.noreply.github.com>
2021-02-15 16:56:19 -0600

    First commit

---
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,62 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+@RequestMapping("hello")
+public class HelloController {
+
+    // handles request a path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//
+//        return "Hello, Spring";
+//    }
+    //hello/goodbye
+    @GetMapping("goodbye")
+
+    public String goodbye(){
+
+        return "Goodbye";
+    }
+
+    //create handler request from /hello?name=Launched
+
+    @RequestMapping(method={RequestMethod.GET,RequestMethod.POST})
+
+    public String helloWithQueryParam(@RequestParam String name){
+
+        return "Hello, " + name + " !";
+    }
+
+    //Handles request from hello/Launchcode
+
+    @GetMapping("/{name}")
+
+    public String helloPathParam(@PathVariable String name){
+
+        return "Hello, " + name + " !";
+    }
+
+
+    @GetMapping("form")
+
+    public String helloForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action= 'hello' method='post'>" + // submit a request to hello
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+
+    }
+
+
+
+}
+
